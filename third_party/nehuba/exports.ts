86f7e09d55dea4361fb77fc8e5a4e21e2be81ac1
Xiao Gui <xgui3783@gmail.com>
2017-08-17 08:58:25 +0200

    chore, removed nehubaviewer, neuroglancer from /thirdparty

--- third_party/nehuba/exports.ts
+++
@@ -1,4 +0,0 @@
-export { vec3, vec4, quat } from 'neuroglancer/util/geom';
-
-export { Config, removeBackgroundMode } from './config';
-export { createNehubaViewer, NehubaViewer } from './NehubaViewer';
