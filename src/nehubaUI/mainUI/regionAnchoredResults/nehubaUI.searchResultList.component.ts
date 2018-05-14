93ede474d9e8cd6f6d72e2bd4eedb22c15f374d4
Xiao Gui <xgui3783@gmail.com>
2018-05-14 09:01:53 +0200

    when no region is selected search results are categorised by region, rather than list

--- src/nehubaUI/mainUI/regionAnchoredResults/nehubaUI.searchResultList.component.ts
+++ src/nehubaUI/mainUI/regionAnchoredResults/nehubaUI.searchResultList.component.ts
@@ -6,6 +6,8 @@ import { SearchResultInterface } from 'nehubaUI/mainUI/searchResultUI/searchResu
 
 import template from './nehubaUI.searchResultList.template.html'
 import css from './nehubaUI.searchResultList.style.css'
+import { RegionDescriptor } from 'nehubaUI/nehuba.model';
+import { MainController } from 'nehubaUI/nehubaUI.services';
 
 @Component({
   selector : 'nehubaui-searchresult-ui-list',
@@ -21,15 +23,26 @@ export class SearchResultUIList implements OnChanges{
   @Input() searchResultObjects : SearchResultInterface[] = []
   @Input() filterSearchResultbyType : {name : string, enabled : boolean}[] = []
 
+  allLabelledRegions : RegionDescriptor[] = []
+
+  constructor(private mainController:MainController){
+    this.allLabelledRegions = Array.from(this.mainController.regionsLabelIndexMap.values())
+  }
+
   ngOnChanges(){
     this.currentPage = Math.max(0,Math.min(this.currentPage,Math.ceil(this.total / this.hitsPerPage) - 1))
+  }
+
+  renderRegionName(obj:{region:RegionDescriptor|null,searchResult:SearchResultInterface[]}){
+    return `${obj.region ? obj.region.name : 'Unlinked to any region'} <small class = "text-muted">(${obj.searchResult.length})</small>`
   }
 
   currentPage : number = 0
   hitsPerPage : number = 15
   get total(){
-    const newFilterDatasetSearchResult = new FilterDatasetSearchResult()
-    return newFilterDatasetSearchResult.transform( this.searchResultObjects,this.filterSearchResultbyType ).length
+    // const newFilterDatasetSearchResult = new FilterDatasetSearchResult()
+    const groupDatasetByRegion = new GroupDatasetByRegion()
+    return groupDatasetByRegion.transform( this.searchResultObjects,this.filterSearchResultbyType,this.allLabelledRegions ).length
   }
   paginationChange(pgnum:number){
     this.currentPage = pgnum
@@ -61,4 +74,48 @@ export class FilterDatasetSearchResult implements PipeTransform{
       return filter ? filter.enabled : false
     })
   }
+}
+
+@Pipe({
+  name : 'groupDatasetByRegion'
+})
+export class GroupDatasetByRegion implements PipeTransform{
+  public transform(datasets:SearchResultInterface[],filterArr:{name:string,enabled:boolean}[],regions:RegionDescriptor[]):{region:RegionDescriptor|null,searchResult:SearchResultInterface[]}[]{
+    const pipe = new FilterDatasetSearchResult()
+    const filteredSearchresults = pipe.transform(datasets,filterArr)
+    
+    return filteredSearchresults.reduce((acc,curr)=>{
+      return (curr.regionName && curr.regionName.length > 0) ?
+        curr.regionName.reduce((acc2,reName)=>{
+          const idx = acc
+            .findIndex(it => it.region === null ? 
+              reName.regionName === 'none' :
+              it.region.name === reName.regionName )
+
+          return idx >= 0 ? 
+            acc2.map((v,i)=> i === idx ? Object.assign({},v,{searchResult : v.searchResult.concat(curr)}) : v ) :
+            acc2.concat({
+              region : this.getRegionFromRegionName(reName.regionName, regions),
+              searchResult : [ curr ]
+            })
+        },acc) :
+        acc.findIndex(it=>it.region==null) >= 0 ?
+          acc.map(it=>it.region === null ? 
+            Object.assign({},it,{
+              searchResult:it.searchResult.concat(curr)
+            }) : 
+          it) : 
+          acc.concat({
+            region : null,
+            searchResult : [curr]
+          })
+        
+    },[] as {region:RegionDescriptor|null,searchResult:SearchResultInterface[]}[])
+  }
+
+  private getRegionFromRegionName(regionName:string,regions:RegionDescriptor[]):RegionDescriptor|null{
+    const idx =  regions.findIndex(re=>re.name == regionName) 
+    return idx >= 0 ? regions[idx] : null
+  }
+
 }
