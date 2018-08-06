ee92d0b14923f78f8f4544f83c63b59232881384
Xiao Gui <xgui3783@gmail.com>
2018-08-06 14:24:53 +0200

    WIP

---
+++ src/util/pipes/copyProperty.pipe.spec.js
@@ -0,0 +1,30 @@
+// import { CopyPropertyPipe } from './copyProperty.pipe'
+// import {describe,it,expect} from 'jasmine'
+import { TempClass } from './temp'
+
+describe('copyProperty.pipe works as expected', () => {
+  it('jasmine works', () => {
+    expect(1).toBe(1)
+  })
+  // it('pipe should work', () => {
+  //   const pipe = new CopyPropertyPipe()
+  //   const array = [{
+  //     name : 'name1',
+  //     key1 : 'value1'
+  //   },{
+  //     name : 'name2',
+  //     key1 : 'value2'
+  //   }]
+  //   const newItem = pipe.transform(array,'key1','key2')
+  //   // expect(newItem).toContain({
+  //   //   namem : 'name1',
+  //   //   key1 : 'value1',
+  //   //   key2 : 'value1'
+  //   // })
+  //   expect(newItem[0]).toEqual({
+  //     name : 'name1',
+  //     key1 : 'value1',
+  //     key2 : 'value1'
+  //   })
+  // })
+})
