d1bed8b7115fe2843406fe7b752e6e5e2450342b
Xiao Gui <xgui3783@gmail.com>
2018-09-04 15:55:12 +0200

    isolate layer browser, removes its dependency on input

--- src/ui/layerbrowser/layerbrowser.component.ts
+++ src/ui/layerbrowser/layerbrowser.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component,  OnDestroy } from "@angular/core";
 import { NgLayerInterface } from "../../atlasViewer/atlasViewer.component";
+import { Store, select } from "@ngrx/store";
+import { ViewerStateInterface, isDefined, REMOVE_NG_LAYER, FORCE_SHOW_SEGMENT } from "../../services/stateStore.service";
+import { Subscription, Observable } from "rxjs";
+import { filter, distinctUntilChanged, map, delay } from "rxjs/operators";
 
 @Component({
   selector : 'layer-browser',
@@ -7,12 +11,58 @@ import { NgLayerInterface } from "../../atlasViewer/atlasViewer.component";
   styleUrls : [ './layerbrowser.style.css' ]
 })
 
-export class LayerBrowser {
+export class LayerBrowser implements OnDestroy{
 
-  @Input() ngLayers : NgLayerInterface[] = []
-  @Input() lockedLayers : string[] = []
+  ngLayers : NgLayerInterface[] = []
+  lockedLayers : string[] = []
 
-  @Output() removeLayerEmitter : EventEmitter<string> = new EventEmitter()
+  public forceShowSegmentCurrentState : boolean | null = null
+  public forceShowSegment$ : Observable<boolean|null>
+  private subscriptions : Subscription[] = []
+  private disposeHandler : any
+
+  constructor(private store : Store<ViewerStateInterface>){
+
+    this.forceShowSegment$ = this.store.pipe(
+      select('ngViewerState'),
+      filter(state => isDefined(state) && typeof state.forceShowSegment !== 'undefined'),
+      map(state => state.forceShowSegment)
+    )
+
+    this.subscriptions.push(this.store.pipe(
+      select('viewerState'),
+      filter(state => isDefined(state) && isDefined(state.templateSelected)),
+      distinctUntilChanged((o,n) => o.templateSelected.name === n.templateSelected.name),
+      map(state => Object.keys(state.templateSelected.nehubaConfig.dataset.initialNgState.layers)),
+      delay(0)
+    ).subscribe((lockedLayerNames:string[]) => {
+
+      this.lockedLayers = lockedLayerNames
+
+      this.ngLayersChangeHandler()
+      this.disposeHandler = window['viewer'].layerManager.layersChanged.add(() => this.ngLayersChangeHandler())
+      window['viewer'].registerDisposer(this.disposeHandler)
+    }))
+
+    this.subscriptions.push(this.forceShowSegment$.subscribe(state => {
+      console.log('assigning new val')
+      this.forceShowSegmentCurrentState = state
+    }))
+  }
+
+  ngOnDestroy(){
+    this.subscriptions.forEach(s => s.unsubscribe())
+  }
+
+  ngLayersChangeHandler(){
+
+    this.ngLayers = (window['viewer'].layerManager.managedLayers as any[]).map(obj => ({
+      name : obj.name,
+      type : obj.initialSpecification.type,
+      source : obj.sourceUrl,
+      visible : obj.visible
+    }) as NgLayerInterface)
+  }
 
   public classVisible(layer:any):boolean{
     return typeof layer.visible === 'undefined'
@@ -28,12 +78,45 @@ export class LayerBrowser {
       return this.lockedLayers.findIndex(l => l === ngLayer.name) >= 0
   }
 
+  toggleVisibility(layer:any){
+    const layerName = layer.name
+    if(!layerName){
+      console.error('layer name not defined', layer)
+      return
+    }
+    const ngLayer = window['viewer'].layerManager.getLayerByName(layerName)
+    if(!ngLayer){
+      console.error('ngLayer could not be found', layerName, window['viewer'].layerManager.managedLayers)
+    }
+    ngLayer.setVisible(!ngLayer.visible)
+  }
+
+  toggleForceShowSegment(ngLayer:any){
+    if(!ngLayer && ngLayer.type !== 'segmentation'){
+      /* toggle only on segmentation layer */
+      return
+    }
+
+    this.store.dispatch({
+      type : FORCE_SHOW_SEGMENT,
+      forceShowSegment : this.forceShowSegmentCurrentState === null
+        ? true
+        : this.forceShowSegmentCurrentState === true
+          ? false
+          : null
+    })
+  }
+
   removeLayer(layer:any){
     if(this.checkLocked(layer)){
       console.warn('this layer is locked and cannot be removed')
-    }else{
-      console.log(layer)
-      // this.removeLayerEmitter.emit()
+      return
     }
+    this.store.dispatch({
+      type : REMOVE_NG_LAYER,
+      layer : {
+        name : layer.name
+      }
+    })
   }
 }
