75acce4165677f90ff9dec1fd0e50263e8268244
Xiao Gui <xgui3783@gmail.com>
2018-09-05 14:51:50 +0200

    e2e tests init

---
+++ e2e/protractor.conf.js
@@ -0,0 +1,8 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['./src/**/*.e2e-spec.js'],
+  // params: {
+  //   interactiveViewer : 'interactiveViewer',
+  //   viewer: 'viewer'
+  // }
+}
