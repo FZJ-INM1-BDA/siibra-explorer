b43427bcaf529135ab56e40424d85c75ae02da47
Xiao Gui <xgui3783@gmail.com>
2018-09-13 10:14:49 +0200

    chore: house keeping, adding loading indicator to index.html

--- src/main-aot.ts
+++ src/main-aot.ts
@@ -2,10 +2,14 @@ import 'zone.js'
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { MainModule } from './main.module';
+import { enableProdMode } from '@angular/core';
 
 const requireAll = (r:any) => {r.keys().forEach(r)}
 requireAll(require.context('./res/ext', false, /\.json$/))
 requireAll(require.context('./res/images', true, /\.jpg|\.png/))
 requireAll(require.context('./plugin_examples/jugex', false))
+
+/* aot = production mode */
+enableProdMode()
 
 platformBrowserDynamic().bootstrapModule(MainModule)
