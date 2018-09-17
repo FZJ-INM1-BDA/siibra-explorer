73a02caf09fa7a0a475634dbcc86b0ff7135d7c3
Xiao Gui <xgui3783@gmail.com>
2018-09-17 12:11:45 +0200

    catch error when user enters invalid regex in search region

--- src/util/pipes/filterNameBySearch.pipe.ts
+++ src/util/pipes/filterNameBySearch.pipe.ts
@@ -7,6 +7,11 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 export class FilterNameBySearch implements PipeTransform{
   public transform(searchFields:string[],searchTerm:string){
-    return searchFields.some(searchField=> new RegExp(searchTerm,'i').test(searchField))
+    try{
+      return searchFields.some(searchField=> new RegExp(searchTerm,'i').test(searchField))
+    }catch(e){
+      /* https://stackoverflow.com/a/9310752/6059235 */
+      return searchFields.some(searchField => new RegExp(searchTerm.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')).test(searchField))
+    }
   }
 }
