c7c3af98efc71ca2111fa2f02217c8a30742302f
Xiao Gui <xgui3783@gmail.com>
2018-10-08 16:16:24 +0200

    feat: reworked mainside component.

--- src/layouts/mainside/mainside.animation.ts
+++ src/layouts/mainside/mainside.animation.ts
@@ -7,19 +7,19 @@ export const mainSideAnimation = trigger('collapseSide',[
       'flex-basis' : '0px',
       'width' : '0px'
     }),
-    { params : { sideWidth : 0 } }
+    { params : { sideWidth : 0, animationTiming: 180 } }
   ),
   state('visible',
     style({
       'flex-basis' : '{{ sideWidth }}px',
       'width' : '{{ sideWidth }}px'
     }),
-    { params : { sideWidth : 300 } }
+    { params : { sideWidth : 300, animationTiming: 180 } }
   ),
   transition('collapsed => visible',[
-    animate('180ms ease-out')
+    animate('{{ animationTiming }}ms ease-out')
   ]),
   transition('visible => collapsed',[
-    animate('180ms ease-in')
+    animate('{{ animationTiming }}ms ease-in')
   ])
 ])
