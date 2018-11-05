2e97e50609ddeb0fd222d69927ab8391f540ae04
Xiao Gui <xgui3783@gmail.com>
2018-11-05 10:28:37 +0100

    chore: fixing sample plugin

--- src/plugin_examples/testPlugin/script.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-  setTimeout(()=>{
-    const el = document.getElementById('testplugin-id')
-    const newel = document.createElement('div')
-    newel.innerHTML = `hello new owrld`
-    el.appendChild(newel)
-    
-  },100)
-})()
