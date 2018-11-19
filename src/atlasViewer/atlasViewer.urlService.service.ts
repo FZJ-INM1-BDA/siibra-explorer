c276c5feeafa23501f23080eadbe8f7605a584f2
Xiao Gui <xgui3783@gmail.com>
2018-11-19 11:57:16 +0100

    chore: prettify

--- src/atlasViewer/atlasViewer.urlService.service.ts
+++ src/atlasViewer/atlasViewer.urlService.service.ts
@@ -200,7 +200,7 @@ export class AtlasViewerURLService{
       })
 
       url.search = search.toString()
-      history.replaceState(null,'',url.toString())
+      history.replaceState(null, '', url.toString())
     })
   }
 }
