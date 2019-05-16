bcdf249000bd152ae210a5a2224c09caf3399333
fsdavid <daviti1@mail.com>
2019-05-16 17:15:59 +0200

    upgraded for mobile context menu etc.

--- src/atlasViewer/atlasViewer.component.ts
+++ src/atlasViewer/atlasViewer.component.ts
@@ -459,6 +459,12 @@ export class AtlasViewer implements OnDestroy, OnInit, AfterViewInit {
       type: TOGGLE_SIDE_PANEL
     })
   }
+
+  onSwipeLeft(event) {
+    this.store.dispatch({
+      type: CLOSE_SIDE_PANEL
+    })
+  }
 }
 
 export interface NgLayerInterface{
