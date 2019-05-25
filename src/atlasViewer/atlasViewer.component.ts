a3c8c7b83efc8f20632963b0cb62f8c75314d7c0
Davit Gogshelidze <gogshela@live.ru>
2019-05-25 12:44:55 +0200

    removed hammerjs and swipe

--- src/atlasViewer/atlasViewer.component.ts
+++ src/atlasViewer/atlasViewer.component.ts
@@ -459,12 +459,6 @@ export class AtlasViewer implements OnDestroy, OnInit, AfterViewInit {
       type: TOGGLE_SIDE_PANEL
     })
   }
-
-  onSwipeLeft(event) {
-    this.store.dispatch({
-      type: CLOSE_SIDE_PANEL
-    })
-  }
 }
 
 export interface NgLayerInterface{
