f2826639a77f0fee663c68fd1118d33ca756a405
Daviti Gogshelidze <daviti1@mail.com>
2019-06-18 10:52:16 +0200

    corrections of indo buttons and download references

--- src/services/zipFileDownload.service.ts
+++ src/services/zipFileDownload.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {AtlasViewerConstantsServices} from "src/atlasViewer/atlasViewer.constantService.service";
+import {map} from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class ZipFileDownloadService {
 
     constructor(private httpClient: HttpClient, private constantService: AtlasViewerConstantsServices) {}
 
-    downloadZip(publicationsText, fileName) {
+    downloadZip(publicationsText, fileName, niiFiles) {
         const correctedName = fileName.replace(/[|&;$%@"<>()+,/]/g, "")
-        this.httpClient.post(this.constantService.backendUrl + 'datasets/downloadParcellationThemself', {
+        return this.httpClient.post(this.constantService.backendUrl + 'datasets/downloadParcellationThemself', {
                 fileName: correctedName,
                 publicationsText: publicationsText,
+                niiFiles: niiFiles === 0 ? null : niiFiles
             },{responseType: "text"}
-        ).subscribe(data => {
-            this.downloadFile(data, correctedName)
-        })
+        ).pipe(
+            map (data => {
+                this.downloadFile(data, correctedName)
+            })
+        )
     }
 
     downloadFile(data, fileName) {
-
         const contentType = 'application/zip';
         const b64Data = data
-
 
         const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
             const byteCharacters = atob(b64Data);
@@ -44,9 +46,7 @@ export class ZipFileDownloadService {
             return blob;
         }
 
-
         const blob = b64toBlob(b64Data, contentType);
-        // const blob = new Blob([data], { type: 'text/csv' });
         const url= window.URL.createObjectURL(blob);
         const anchor = document.createElement("a");
         anchor.download = fileName + '.zip';
