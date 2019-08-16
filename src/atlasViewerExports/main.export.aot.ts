dbad59b0cc515b86ed16fe09ece53ac6aa936016
Xiao Gui <xgui3783@gmail.com>
2019-08-16 18:10:56 +0200

    add theme

--- src/atlasViewerExports/main.export.aot.ts
+++ src/atlasViewerExports/main.export.aot.ts
@@ -1,5 +1,8 @@
 import 'zone.js'
 
+import './theme.scss'
+import './res/css/extra_styles.css'
+
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { ExportModule } from "./export.module";
 
