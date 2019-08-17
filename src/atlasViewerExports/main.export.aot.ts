be86ff2e73402d08ef48cbee9e86fc26e8d03e3f
Xiao Gui <xgui3783@gmail.com>
2019-08-17 12:41:57 +1000

    fix theming import

--- src/atlasViewerExports/main.export.aot.ts
+++ src/atlasViewerExports/main.export.aot.ts
@@ -1,8 +1,5 @@
 import 'zone.js'
 
-import './theme.scss'
-import './res/css/extra_styles.css'
-
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { ExportModule } from "./export.module";
 
