7bbc3ce95fda8e2bdd6f56d76fcfac8f97878765
Xiao Gui <xgui3783@gmail.com>
2019-09-30 18:09:36 +0200

    feat: minor ui improve on modality filter

---
+++ src/util/directives/delayEvent.directive.ts
@@ -0,0 +1,49 @@
+import { Directive, Input, OnChanges, OnDestroy, ElementRef, Output, EventEmitter } from "@angular/core";
+
+const VALID_EVENTNAMES = new Set([
+  'mousedown',
+  'mouseup',
+  'click',
+  'mouseenter',
+  'mouseleave',
+  'touchstart',
+  'touchmove',
+  'touchend'
+])
+
+@Directive({
+  selector: '[iav-delay-event]'
+})
+
+export class DelayEventDirective implements OnChanges, OnDestroy {
+
+  private evListener = (ev:Event) => setTimeout(() => this.delayedEmit.emit(ev))
+
+  @Input('iav-delay-event')
+  delayEvent: string = ''
+
+  @Output()
+  delayedEmit: EventEmitter<any> = new EventEmitter()
+
+  constructor(private el: ElementRef){
+
+  }
+
+  private destroyCb: (() => void)[] = []
+  ngOnChanges(){
+
+    const el = this.el.nativeElement as HTMLElement
+    for (const evName of this.delayEvent.split(' ')){
+      if (VALID_EVENTNAMES.has(evName)) {
+        el.addEventListener(evName, this.evListener)
+        this.destroyCb.push(() => el.removeEventListener(evName, this.evListener))
+      } else {
+        console.warn(`${evName} is not a valid event name in the supported set`, VALID_EVENTNAMES)
+      }
+    }
+  }
+
+  ngOnDestroy(){
+    while(this.destroyCb.length > 0) this.destroyCb.pop()()
+  }
+}
