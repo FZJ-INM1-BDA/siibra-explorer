b97af9e4dc0a706e24e9cc994fa2ad1b42ccedeb
Xiao Gui <xgui3783@gmail.com>
2020-01-06 11:38:10 +0100

    WIP triage

---
+++ deploy/datasets/testData/allen2015.js
@@ -0,0 +1,113 @@
+module.exports = [
+  {
+    "formats": [],
+    "datasetDOI": [
+      {
+        "cite": "Holmseth, S., Scott, H. A., Real, K., Lehre, K. P., Leergaard, T. B., Bjaalie, J. G., & Danbolt, N. C. (2009). The concentrations and distributions of three C-terminal variants of the GLT1 (EAAT2; slc1a2) glutamate transporter protein in rat brain tissue suggest differential regulation. Neuroscience, 162(4), 1055–1071. ",
+        "doi": "10.1016/j.neuroscience.2009.03.048"
+      }
+    ],
+    "activity": [
+      {
+        "protocols": [
+          "Immunohistochemistry",
+          "Atlas",
+          "Brain-wide",
+          "Synaptic transmission",
+          "Neurtransmitter transport",
+          "Glutamate uptake",
+          "GLT1"
+        ],
+        "preparation": [
+          "Ex vivo"
+        ]
+      }
+    ],
+    "referenceSpaces": [],
+    "methods": [
+      "Immunohistochemistry"
+    ],
+    "custodians": [
+      "Danbolt,  Niels C."
+    ],
+    "project": [
+      "Rodent brain neurotransporter atlas: GLT1"
+    ],
+    "description": "Glutamate is the major excitatory transmitter in the central nervous system (Danbolt, Prog. Neurobiol. 65:1-105, 2001). It is inactivated by cellular uptake, mostly catalyzed by the glutamate transporters GLT1 (slc1a2, excitatory amino acid transporter [EAAT2]) subtype expressed at high levels in brain astrocytes and at lower levels in neurons. Three C-terminal variants of EAAT2 exist: GLT1a (Pines et al., Nature 360:464-467, 1992), GLT1b (Utsunomiya-Tate et al., FEBS Lett 416:312-326,1997), and GLT1c (Rauen et al., Neurochem. Int. 45:1095-1106, 2004). This dataset is brain-wide collection of microscopic images showing the brain-wide distribution of GLT1 in the mouse and rat brain, visualized by immunohistochemistry using antibodies against GLT1a and GLT1b. To facilitate identification of anatomical location adjacent section were stained to reveal cyto- and myeloarchitecture.",
+    "parcellationAtlas": [
+      {
+        "name": "Allen Mouse Common Coordinate Framework v3 2015",
+        "fullId": "https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationatlas/v1.0.0/39a1384b-8413-4d27-af8d-22432225401f",
+        "id": "39a1384b-8413-4d27-af8d-22432225401f"
+      },
+      {
+        "name": "Waxholm Space rat brain atlas v2",
+        "fullId": "https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationatlas/v1.0.0/2449a7f0-6dd0-4b5a-8f1e-aec0db03679d",
+        "id": "2449a7f0-6dd0-4b5a-8f1e-aec0db03679d"
+      }
+    ],
+    "licenseInfo": [
+      {
+        "name": "Creative Commons Attribution-ShareAlike 4.0 International",
+        "url": "https://creativecommons.org/licenses/by-sa/4.0"
+      }
+    ],
+    "embargoStatus": [
+      "Free"
+    ],
+    "license": [
+      {
+        "name": "Creative Commons Attribution-ShareAlike 4.0 International",
+        "relativeUrl": "minds/core/licensetype/v1.0.0/78a3bfb2-f4b9-40f0-869c-34b5e48a45bd"
+      }
+    ],
+    "parcellationRegion": [
+      {
+        "species": [],
+        "name": "Mouse Whole brain (v3 2015)",
+        "alias": "Whole brain"
+      },
+      {
+        "species": [
+          {
+            "identifier": [
+              "e9a384ea8a4edf817710b6edef5f2940",
+              "5401fdb1d638c2bc5b68241560cddac0"
+            ],
+            "name": "Rattus norvegicus",
+            "@id": "https://nexus.humanbrainproject.org/v0/data/minds/core/species/v1.0.0/f3490d7f-8f7f-4b40-b238-963dcac84412"
+          }
+        ],
+        "name": "Whole Brain ",
+        "alias": "Whole brain"
+      }
+    ],
+    "species": [
+      "Rattus norvegicus",
+      "Mus musculus"
+    ],
+    "name": "Brain-wide distribution of glutamate type 1 transporter protein (GLT1)",
+    "files": [],
+    "fullId": "https://nexus.humanbrainproject.org/v0/data/minds/core/dataset/v1.0.0/f7a7d460-8724-4cd1-a06e-457eb8954fbd",
+    "contributors": [
+      "Danbolt, Nils C.",
+      "Bjaalie, Jan G.",
+      "Leergaard, Trygve B.",
+      "Lehre, K.P.",
+      "Real, Katia",
+      "Scott, Heather A.",
+      "Holmseth, Silvia"
+    ],
+    "id": "63bbb845ac6d2f1839f919c2ef0455bc",
+    "kgReference": [
+      "10.25493/Y147-2CE"
+    ],
+    "publications": [
+      {
+        "name": "The concentrations and distributions of three C-terminal variants of the GLT1 (EAAT2; slc1a2) glutamate transporter protein in rat brain tissue suggest differential regulation",
+        "cite": "Holmseth, S., Scott, H. A., Real, K., Lehre, K. P., Leergaard, T. B., Bjaalie, J. G., & Danbolt, N. C. (2009). The concentrations and distributions of three C-terminal variants of the GLT1 (EAAT2; slc1a2) glutamate transporter protein in rat brain tissue suggest differential regulation. Neuroscience, 162(4), 1055–1071. ",
+        "doi": "10.1016/j.neuroscience.2009.03.048"
+      }
+    ]
+  }
+]
--- deploy/datasets/util.spec.js
+++ deploy/datasets/util.spec.js
@@ -1,10 +1,73 @@
-const { retry } = require('./util')
+const { retry, filterDatasets, datasetRegionExistsInParcellationRegion, _getParcellations } = require('./util')
+const { fake } = require('sinon')
+const { assert, expect } = require('chai')
+const waxholmv2 = require('./testData/waxholmv2')
 
-let val = 0
+describe('datasets/util.js', () => {
 
-const prFn = () => {
-  val++
-  return val >=3 ? Promise.resolve() : Promise.reject()
-} 
+  // describe('retry', () => {
 
-retry(() => prFn())
+  //   let val = 0
+  
+  //   const failCall = fake()
+  //   const succeedCall = fake()
+  
+  //   const prFn = () => {
+  //     val++
+  //     return val >=3
+  //       ? (succeedCall(), Promise.resolve())
+  //       : (failCall(), Promise.reject())
+  //   }
+  
+  //   beforeEach(() => {
+  //     val = 0
+  //     succeedCall.resetHistory()
+  //     failCall.resetHistory()
+  //   })
+  
+  //   it('retry until succeed', async () => {
+  //     await retry(prFn)
+  //     assert(succeedCall.called)
+  //     assert(failCall.calledTwice)
+  //   })
+  
+  //   it('retry with shorter timeouts', async () => {
+  //     await retry(prFn, { timeout: 100 })
+  //     assert(succeedCall.called)
+  //     assert(failCall.calledTwice)
+  //   })
+  
+  //   it('when retries excceeded, retry fn throws', async () => {
+  //     try {
+  //       await retry(prFn, { timeout: 100, retries: 2 })
+  //       assert(false, 'retry fn should throw if retries exceed')
+  //     } catch (e) {
+  //       assert(true)
+  //     }
+  //   })
+  // })
+  
+  describe('datasetRegionExistsInParcellationRegion', () => {
+    it('should filter waxholm v2 properly', async () => {
+
+      const waxholmv2Pr = waxholmv2.map(dataset => {
+        return dataset.parcellationRegion
+      })
+
+      const { waxholm2Set } = await _getParcellations()
+      for (const pr of waxholmv2Pr){
+
+      const flag = await datasetRegionExistsInParcellationRegion(pr, waxholm2Set)
+        expect(flag).to.be.true
+      }
+    })
+  })
+  
+  describe('filterDatasets', () => {
+    it('should filter waxholm v2 properly', async () => {
+      const filteredResult = await filterDatasets(waxholmv2, { parcellationName: 'Waxholm Space rat brain atlas v2' })
+      expect(filteredResult).to.have.length(1)
+    })
+  })
+  
+})
