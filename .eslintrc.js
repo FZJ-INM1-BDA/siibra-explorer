bd5c56226056514c68a6beacaaa878550f0c2018
Xiao Gui <xgui3783@gmail.com>
2020-01-08 14:33:52 +0100

    chore: update lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     }],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-use-before-define": "off"
+    "@typescript-eslint/no-use-before-define": "off",
+    "no-extra-boolean-cast": "off"
   }
 };
