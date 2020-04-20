8d84c5f3bb1820fb9766bc9d3d46795ae93b27ef
Xiao Gui <xgui3783@gmail.com>
2020-04-20 14:30:49 +0200

    bugfix: selected region not persistent via URL

--- src/ui/nehubaContainer/nehubaViewerInterface/nehubaViewerInterface.directive.ts
+++ src/ui/nehubaContainer/nehubaViewerInterface/nehubaViewerInterface.directive.ts
@@ -191,7 +191,43 @@ const accumulatorFn: (
   return newMap
 }
 
-// TODO port viewer related functionalities (input/outputs) from nehubacontainer to here!
+// methods
+//
+// new viewer
+// change state (layer visibliity)
+// change state (segment visibility)
+// change state (color map)
+// change state (add/remove layer)
+// changeNavigation
+// setLayout (2x2 or max screen)
+
+// emitters
+//
+// mouseoverSegments
+// mouseoverLandmarks
+// selectSegment
+// navigationChanged
+
+/**
+ * This directive should only deal with non-navigational interface between
+ * - viewer (nehuba)
+ * - state store (ngrx)
+ * 
+ * 
+ * public prop
+ * 
+ * - newViewer (new template / null for destroying current instance)
+ * - segmentVisibility change
+ * - setColorMap for segmentation map
+ * - add/remove layer (image/segmentation/mesh)
+ * 
+ * emitters
+ * 
+ * - mouseoverSegments
+ * - mouseoverLandmark
+ * - selectSegment
+ * - loadingStatus
+ */
 
 @Directive({
   selector: '[iav-nehuba-viewer-container]',
@@ -208,7 +244,7 @@ export class NehubaViewerContainerDirective implements OnInit, OnDestroy{
   constructor(
     private el: ViewContainerRef,
     private cfr: ComponentFactoryResolver,
-    private store$: Store<IavRootStoreInterface>
+    private store$: Store<IavRootStoreInterface>,
   ){
     this.nehubaViewerFactory = this.cfr.resolveComponentFactory(NehubaViewerUnit)
 
@@ -293,6 +329,12 @@ export class NehubaViewerContainerDirective implements OnInit, OnDestroy{
   }
 
   createNehubaInstance(template: any){
+    
+    this.store$.dispatch({
+      type: NEHUBA_READY,
+      nehubaReady: false,
+    })
+
     this.clear()
     this.iavNehubaViewerContainerViewerLoading.emit(true)
     this.cr = this.el.createComponent(this.nehubaViewerFactory)
