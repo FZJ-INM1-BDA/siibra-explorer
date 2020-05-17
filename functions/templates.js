18a17ad6d59acbdad71611a3565eddddec4ffbf2
Xiao Gui <xgui3783@gmail.com>
2020-05-17 16:58:36 +0200

    udpate netlify functions

--- functions/templates.js
+++ functions/templates.js
@@ -1,7 +1,28 @@
 exports.handler = (ev, ctx, cb) => {
+  
+  const {
+    path,
+    httpMethod,
+    headers,
+    queryStringParameters,
+    body,
+    isBase64Encoded,
+  } = ev
+
+  const templates = [
+    // 'infant',
+    'bigbrain',
+    'colin',
+    'MNI152',
+    'waxholmRatV2_0',
+    'allenMouse'
+  ]
+
   const resp = {
-    hello: 'world'
+    templates,
+    ev
   }
+  
   cb(null, {
     status: 400,
     body: JSON.stringify(resp),
