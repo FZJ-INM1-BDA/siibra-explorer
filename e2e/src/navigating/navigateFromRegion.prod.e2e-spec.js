1b058071fe7581f34e14b0cf317d5b319f291ddc
fsdavid <daviti1@mail.com>
2020-05-28 09:02:10 +0200

    check region menu explore template button attribute test case

--- e2e/src/navigating/navigateFromRegion.prod.e2e-spec.js
+++ e2e/src/navigating/navigateFromRegion.prod.e2e-spec.js
@@ -134,12 +134,12 @@ describe('> explore same region in different templates', () => {
 
   describe('> menu UI', () => {
     const data = TEST_DATA[0]
-    
+
     beforeEach(async () => {
-      await getBeforeEachFn(iavPage)(data)()
     })
 
     it('> dismisses when user clicks/drags outside', async () => {
+      await getBeforeEachFn(iavPage)(data)()
       const { expectedRegion, expectedTemplateLabels, position, url, templateName } = data
 
       await iavPage.cursorMoveToAndDrag({
@@ -155,6 +155,20 @@ describe('> explore same region in different templates', () => {
         expect(true).toBe(false)
       } catch(e) {
         expect(true).toBe(true)
+      }
+    })
+
+    it('> Tooltip visible if overflowed', async () => {
+      const data2 = TEST_DATA[1]
+      await getBeforeEachFn(iavPage)(data2)()
+
+      const {expectedRegion, expectedTemplateLabels, position, url, templateName} = data2
+
+      const desiredTemplateButton = await expectedTemplateLabels.find(el => el.name.length > 30)
+
+      if (desiredTemplateButton) {
+        const test = await iavPage.getAttribute(`[aria-label="${SHOW_IN_OTHER_REF_SPACE}: ${desiredTemplateButton.name}${desiredTemplateButton.hemisphere ? (' - ' + desiredTemplateButton.hemisphere) : ''}"]`, 'matTooltip')
+        console.log(test)
       }
     })
   })
