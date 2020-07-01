80436738c92103267000f6a9cf8ad21068cc1583
Xiao Gui <xgui3783@gmail.com>
2020-07-01 14:24:19 +0200

    feat: atlas layer widgets

---
+++ e2e/src/layout/atlasWidgetBtns.prod.e2e-spec.js
@@ -0,0 +1,40 @@
+/**
+ * TODO finish tests!
+ */
+
+describe('> atlas widget btns', () => {
+  it('> fails unless tests are written', () => {
+    expect(true).toEqual(false)
+  })
+  describe('> when default layer is selected', () => {
+    it('> hierarchy btn should show hierarchy', () => {
+
+    })
+
+    it('> connectivity btn should show connectivity', () => {
+
+    })
+
+    it('> dataset should show dataset', () => {
+
+    })
+  })
+
+  describe('> when another layer is displayed', () => {
+    it('> mat-chip should update', () => {
+
+    })
+
+    it('> cicking close on mat chip should result in removing additional layer', () => {
+
+    })
+
+    it('> hierarchy should be updated', () => {
+
+    })
+
+    it('> databrowser should be updated', () => {
+      
+    })
+  })
+})
