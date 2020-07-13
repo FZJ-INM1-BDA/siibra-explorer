1abb9bc51c06f36b47ae7da9bf5cd3ec3d1856e1
Xiao Gui <xgui3783@gmail.com>
2020-07-13 10:54:09 +0200

    feat: implement ready probe for auth

--- deploy/auth/index.js
+++ deploy/auth/index.js
@@ -1,31 +1,34 @@
-const hbpOidc = require('./hbp-oidc')
-const hbpOidc2 = require('./hbp-oidc-v2')
-const passport = require('passport')
 const objStoreDb = new Map()
 const HOST_PATHNAME = process.env.HOST_PATHNAME || ''
+const { retry } = require('../../common/util')
 
-module.exports = async (app) => {
-  app.use(passport.initialize())
-  app.use(passport.session())
+let isReady = false
 
-  passport.serializeUser((user, done) => {
-    const { tokenset, rest } = user
-    objStoreDb.set(user.id, user)
-    done(null, user.id)
-  })
+/**
+ * using async function. Maybe in the future, we want to introduce async checks
+ */
+const ready = async () => isReady
 
-  passport.deserializeUser((id, done) => {
-    const user = objStoreDb.get(id)
-    if (user) return done(null, user)
-    else return done(null, false)
-  })
+const configureAuth = async (app) => {
+  const hbpOidc = require('./hbp-oidc')
+  const hbpOidc2 = require('./hbp-oidc-v2')
+  
+  const obj = await require('./util')()
+  const { initPassportJs } = obj
+  initPassportJs()
 
-  await hbpOidc(app)
-  await hbpOidc2(app)
+  await retry(() => hbpOidc(app), { timeout: 1000, retries: 3 })
+  await retry(() => hbpOidc2(app), { timeout: 1000, retries: 3 })
+  isReady = true
 
   app.get('/logout', (req, res) => {
     if (req.user && req.user.id) objStoreDb.delete(req.user.id)
     req.logout()
     res.redirect(`${HOST_PATHNAME}/`)
   })
+}
+
+module.exports = {
+  configureAuth,
+  ready
 }
