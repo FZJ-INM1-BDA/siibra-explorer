fb9e22b697ae6d8246e8dacf185cb8ef620cd287
Xiao Gui <xgui3783@gmail.com>
2020-08-31 10:01:32 +0200

    fix: tests

--- src/services/state/ngViewerState/selectors.spec.ts
+++ src/services/state/ngViewerState/selectors.spec.ts
@@ -1,27 +1,27 @@
-import { ngViewerSelectorClearView } from './selectors'
+import { ngViewerSelectorClearViewEntries } from './selectors'
 
 let clearViewQueue = {}
 
 describe('> ngViewerState/selectors.ts', () => {
-  describe('> ngViewerSelectorClearView', () => {
+  describe('> ngViewerSelectorClearViewEntries', () => {
     beforeEach(() => {
       clearViewQueue = {}
     })
     describe('> when prop is not provided', () => {
       it('> if clearViewQueue is empty (on startup)', () => {
-        const result = ngViewerSelectorClearView.projector(clearViewQueue)
-        expect(result).toEqual(false)
+        const result = ngViewerSelectorClearViewEntries.projector(clearViewQueue)
+        expect(result).toEqual([])
       })
       it('> if clearViewQueue is non empty, but falsy, should return false', () => {
         clearViewQueue['hello - world'] = null
         clearViewQueue['oo bar'] = false
-        const result = ngViewerSelectorClearView.projector(clearViewQueue)
-        expect(result).toEqual(false)
+        const result = ngViewerSelectorClearViewEntries.projector(clearViewQueue)
+        expect(result).toEqual([])
       })
       it('> if clearViewQueue is non empty and truthy, should return true', () => {
         clearViewQueue['hello - world'] = 1
-        const result = ngViewerSelectorClearView.projector(clearViewQueue)
-        expect(result).toEqual(true)
+        const result = ngViewerSelectorClearViewEntries.projector(clearViewQueue)
+        expect(result).toEqual(['hello - world'])
       })
     })
   })
