d0cf54b88769f032383e5e3f6b8aeea4ec2c7224
Xiao Gui <xgui3783@gmail.com>
2020-10-30 16:29:21 +0100

    bugfix: multilevel human parc name not shown (#714)

--- e2e/src/selecting/atlas.prod.e2e-spec.js
+++ e2e/src/selecting/atlas.prod.e2e-spec.js
@@ -1,18 +1,43 @@
-/**
- * TODO
- * 
- */
+const { AtlasPage } = require('../../util/helper')
+
+const atlases = [
+  'Waxholm Space atlas of the Sprague Dawley rat brain',
+  'Multilevel Human Atlas',
+  'Allen Mouse Common Coordinate Framework v3'
+]
 
 describe('> atlases are generally available', () => {
-  describe('> on start up, there exist three atlases', () => {
-    describe('> human multi level atlas', () => {
-
-    })
-    describe('> waxholm rat atlas', () => {
-
-    })
-    describe('> allen mouse atlas', () => {
-
-    })
+  let atlasPage = new AtlasPage()
+  beforeEach(async () => {
+    atlasPage = new AtlasPage()
+    await atlasPage.init()
   })
+  it('> on startup, there should be three atlases', async () => {
+    await atlasPage.goto()
+    const texts = await atlasPage.getAllCardsText()
+    expect(texts.sort()).toEqual(atlases.sort())
+  })
+})
+
+describe('> generic atlaes behaviours', () => {
+  let atlasPage = new AtlasPage()
+  beforeEach(async () => {
+    atlasPage = new AtlasPage()
+    await atlasPage.init()
+  })
+  for (const atlas of atlases) {
+    describe(`> of ${atlas}`, () => {
+      it('> on launch, shows atlas name as a pill', async () => {
+        await atlasPage.goto()
+        await atlasPage.clearAlerts()
+        await atlasPage.selectAtlasTemplateParcellation(atlas)
+        await atlasPage.waitUntilAllChunksLoaded()
+        const txtArr = await atlasPage.getAllChipsText()
+        expect(
+          txtArr.filter(v => v.replace(/\s/g, '').length > 0).length
+        ).toBeGreaterThan(0)
+        console.log(`chip shows: ${txtArr.join(', ')}`)
+      })
+    })
+  }
 })
