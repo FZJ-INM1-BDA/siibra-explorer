3122d176149824c1bca85b07fb5d10efa97d5c0d
Xiao Gui <xgui3783@gmail.com>
2020-12-03 16:23:59 +0100

    fix lint

--- src/ui/nehubaContainer/2dLandmarks/landmark.base.ts
+++ src/ui/nehubaContainer/2dLandmarks/landmark.base.ts
@@ -4,8 +4,4 @@ export class LandmarkUnitBase{
   @Input() public positionX: number = 0
   @Input() public positionY: number = 0
   @Input() public positionZ: number = 0
-
-  constructor(){
-    
-  }
 }
