b5e2802529be1284580e9121d29b8d81b6c11d47
Xiao Gui <xgui3783@gmail.com>
2021-08-09 12:50:19 +0200

    feat: allow viewing of local nifti

--- src/viewerModule/nehuba/nehubaViewerGlue/nehubaViewerGlue.component.spec.ts
+++ src/viewerModule/nehuba/nehubaViewerGlue/nehubaViewerGlue.component.spec.ts
@@ -1,27 +1,83 @@
 import { CommonModule } from "@angular/common"
-import { TestBed } from "@angular/core/testing"
+import { Component, Directive } from "@angular/core"
+import { async, ComponentFixture, TestBed } from "@angular/core/testing"
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
 import { MockStore, provideMockStore } from "@ngrx/store/testing"
-import { Subject } from "rxjs"
+import { NEVER, of, Subject } from "rxjs"
+import { ComponentsModule } from "src/components"
 import { ClickInterceptorService } from "src/glue"
+import { LayoutModule } from "src/layouts/layout.module"
 import { PANELS } from "src/services/state/ngViewerState/constants"
 import { ngViewerSelectorOctantRemoval, ngViewerSelectorPanelMode, ngViewerSelectorPanelOrder } from "src/services/state/ngViewerState/selectors"
 import { uiStateMouseOverSegmentsSelector } from "src/services/state/uiState/selectors"
-import { viewerStateSetSelectedRegions } from "src/services/state/viewerState/actions"
+import { viewerStateSetSelectedRegions } from "src/services/state/viewerState.store.helper"
 import { viewerStateCustomLandmarkSelector, viewerStateNavigationStateSelector, viewerStateSelectedRegionsSelector } from "src/services/state/viewerState/selectors"
-import { ClickInterceptor, CLICK_INTERCEPTOR_INJECTOR } from "src/util"
+import { Landmark2DModule } from "src/ui/nehubaContainer/2dLandmarks/module"
+import { QuickTourModule } from "src/ui/quickTour"
+import { AngularMaterialModule } from "src/ui/sharedModules/angularMaterial.module"
+import { ClickInterceptor, CLICK_INTERCEPTOR_INJECTOR, UtilModule } from "src/util"
+import { WindowResizeModule } from "src/util/windowResize"
 import { NehubaLayerControlService } from "../layerCtrl.service"
+import { MaximisePanelButton } from "../maximisePanelButton/maximisePanelButton.component"
 import { NehubaMeshService } from "../mesh.service"
+import { NehubaViewerTouchDirective } from "../nehubaViewerInterface/nehubaViewerTouch.directive"
+import { selectorAuxMeshes } from "../store"
+import { TouchSideClass } from "../touchSideClass.directive"
 import { NehubaGlueCmp } from "./nehubaViewerGlue.component"
+import { HarnessLoader } from "@angular/cdk/testing"
+
+
+@Component({
+  selector: 'viewer-ctrl-component',
+  template: ''
+})
+
+class MockViewerCtrlCmp{}
+
+@Directive({
+  selector: '[iav-nehuba-viewer-container]',
+  exportAs: 'iavNehubaViewerContainer',
+})
+
+class MockNehubaViewerContainerDirective{
+  public viewportToDatas: any
+  public nehubaViewerInstance = {
+    nehubaViewer: {
+      ngviewer: null
+    }
+  }
+  mouseOverSegments = NEVER
+  navigationEmitter = NEVER
+  mousePosEmitter = NEVER
+}
 
 describe('> nehubaViewerGlue.component.ts', () => {
   let mockStore: MockStore
-  beforeEach(() => {
+  let rootLoader: HarnessLoader
+  let fixture: ComponentFixture<NehubaGlueCmp>
+  beforeEach( async(() => {
     TestBed.configureTestingModule({
       imports: [
         CommonModule,
+        AngularMaterialModule,
+        LayoutModule,
+        Landmark2DModule,
+        QuickTourModule,
+        ComponentsModule,
+        UtilModule,
+        WindowResizeModule,
+        FormsModule,
+        ReactiveFormsModule,
       ],
       declarations: [
-        NehubaGlueCmp
+        NehubaGlueCmp,
+        MaximisePanelButton,
+        MockViewerCtrlCmp,
+        TouchSideClass,
+
+        // TODO this may introduce a lot more dep
+        MockNehubaViewerContainerDirective,
+        NehubaViewerTouchDirective,
       ],
       providers: [
         /**
@@ -59,13 +115,8 @@ describe('> nehubaViewerGlue.component.ts', () => {
           }
         }
       ]
-    }).overrideComponent(NehubaGlueCmp, {
-      set: {
-        template: '',
-        templateUrl: null
-      }
     }).compileComponents()
-  })
+  }))
 
   beforeEach(() => {
     mockStore = TestBed.inject(MockStore)
@@ -76,11 +127,13 @@ describe('> nehubaViewerGlue.component.ts', () => {
     mockStore.overrideSelector(viewerStateSelectedRegionsSelector, [])
     mockStore.overrideSelector(uiStateMouseOverSegmentsSelector, [])
     mockStore.overrideSelector(viewerStateNavigationStateSelector, null)
+
+    mockStore.overrideSelector(selectorAuxMeshes, [])
   })
 
 
   it('> can be init', () => {
-    const fixture = TestBed.createComponent(NehubaGlueCmp)
+    fixture = TestBed.createComponent(NehubaGlueCmp)
     expect(fixture.componentInstance).toBeTruthy()
   })
 
@@ -167,6 +220,110 @@ describe('> nehubaViewerGlue.component.ts', () => {
         TestBed.createComponent(NehubaGlueCmp)
         clickIntServ.callRegFns(null)
         expect(fallbackSpy).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('> handleFileDrop', () => {
+    let addNgLayerSpy: jasmine.Spy
+    let removeNgLayersSpy: jasmine.Spy
+    let dummyFile1: File
+    let dummyFile2: File
+    let input: File[]
+
+    beforeEach(() => {
+      dummyFile1 = (() => {
+        const bl: any = new Blob([], { type: 'text' })
+        bl.name = 'filename1.txt'
+        bl.lastModifiedDate = new Date()
+        return bl as File
+      })()
+
+      dummyFile2 = (() => {
+        const bl: any = new Blob([], { type: 'text' })
+        bl.name = 'filename2.txt'
+        bl.lastModifiedDate = new Date()
+        return bl as File
+      })()
+
+      fixture = TestBed.createComponent(NehubaGlueCmp)
+      fixture.detectChanges()
+
+      addNgLayerSpy = spyOn(fixture.componentInstance['layerCtrlService'], 'addNgLayer').and.callFake(() => {
+        
+      })
+      removeNgLayersSpy = spyOn(fixture.componentInstance['layerCtrlService'], 'removeNgLayers').and.callFake(() => {
+        
+      })
+    })
+    afterEach(() => {
+      addNgLayerSpy.calls.reset()
+      removeNgLayersSpy.calls.reset()
+    })
+
+    describe('> malformed input', () => {
+      const scenarios = [{
+        desc: 'too few files',
+        inp: []
+      }, {
+        desc: 'too many files', 
+        inp: [dummyFile1, dummyFile2]
+      }]
+
+      for (const { desc, inp } of scenarios) {
+        describe(`> ${desc}`, () => {
+          beforeEach(() => {
+            input = inp
+  
+            const cmp = fixture.componentInstance
+            cmp.handleFileDrop(input)
+          })
+  
+          it('> should not call addnglayer', () => {
+            expect(removeNgLayersSpy).not.toHaveBeenCalled()
+            expect(addNgLayerSpy).not.toHaveBeenCalled()
+          })
+  
+          // TODO having a difficult time getting snackbar harness
+          // it('> snackbar should show error message', async () => {
+          //   console.log('get harness')
+            
+          //   rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture)
+          //   const loader = TestbedHarnessEnvironment.loader(fixture)
+          //   fixture.detectChanges()
+          //   const snackbarHarness = await rootLoader.getHarness(MatSnackBarHarness)
+          //   console.log('got harness', snackbarHarness)
+          //   // const message = await snackbarHarness.getMessage()
+          //   // console.log('got message')
+          //   // expect(message).toEqual(INVALID_FILE_INPUT)
+          // })
+        })
+      }
+    })
+
+    describe('> correct input', () => {
+      beforeEach(() => {
+        input = [dummyFile1]
+
+        const cmp = fixture.componentInstance
+        cmp.handleFileDrop(input)
+      })
+
+      afterEach(() => {
+        // remove remove all urls
+        fixture.componentInstance['dismissAllAddedLayers']()
+      })
+
+      it('> should call addNgLayer', () => {
+        expect(removeNgLayersSpy).not.toHaveBeenCalled()
+        expect(addNgLayerSpy).toHaveBeenCalledTimes(1)
+      })
+      it('> on repeated input, both remove nglayer and remove ng layer called', () => {
+        const cmp = fixture.componentInstance
+        cmp.handleFileDrop(input)
+
+        expect(removeNgLayersSpy).toHaveBeenCalledTimes(1)
+        expect(addNgLayerSpy).toHaveBeenCalledTimes(2)
       })
     })
   })
