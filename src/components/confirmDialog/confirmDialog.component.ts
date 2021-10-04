340539092390a78961c114064dd49cb8fbb40566
Xiao Gui <xgui3783@gmail.com>
2021-10-04 15:50:50 +0200

    feat: added no. ann badge feat: delete all ann btn feat: loading ann feedback

--- src/components/confirmDialog/confirmDialog.component.ts
+++ src/components/confirmDialog/confirmDialog.component.ts
@@ -25,12 +25,15 @@ export class ConfirmDialogComponent {
   @Input()
   public markdown: string
 
+  public hideActionBar = false
+
   constructor(@Inject(MAT_DIALOG_DATA) data: any) {
-    const { title = null, message  = null, markdown, okBtnText, cancelBtnText} = data || {}
+    const { title = null, message  = null, markdown, okBtnText, cancelBtnText, hideActionBar} = data || {}
     if (title) this.title = title
     if (message) this.message = message
     if (markdown) this.markdown = markdown
     if (okBtnText) this.okBtnText = okBtnText
     if (cancelBtnText) this.cancelBtnText = cancelBtnText
+    if (hideActionBar) this.hideActionBar = hideActionBar
   }
 }
