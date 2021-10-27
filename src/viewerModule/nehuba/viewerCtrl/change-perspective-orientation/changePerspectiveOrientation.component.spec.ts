114c0d054ea0e400938a13a0f58f85115b8a4758
fsdavid <daviti1@mail.com>
2021-10-27 14:44:35 +0200

    Fix change orientation tests

--- src/viewerModule/nehuba/viewerCtrl/change-perspective-orientation/changePerspectiveOrientation.component.spec.ts
+++ src/viewerModule/nehuba/viewerCtrl/change-perspective-orientation/changePerspectiveOrientation.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChangePerspectiveOrientationComponent } from './changePerspectiveOrientation.component';
+import { MockStore, provideMockStore } from "@ngrx/store/testing"
+import { AngularMaterialModule } from 'src/sharedModules';
+import { CommonModule } from '@angular/common';
 
 describe('ChangePerspectiveOrientationComponent', () => {
   let component: ChangePerspectiveOrientationComponent;
   let fixture: ComponentFixture<ChangePerspectiveOrientationComponent>;
+  let mockStore: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangePerspectiveOrientationComponent ]
+      imports: [
+        AngularMaterialModule,
+        CommonModule
+      ],
+      declarations: [ ChangePerspectiveOrientationComponent ],
+      providers: [provideMockStore()],
+
     })
     .compileComponents();
   });
