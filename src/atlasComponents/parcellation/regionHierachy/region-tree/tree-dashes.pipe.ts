512999f7749ba5ca73eea8fa3825f7e58e034b85
fsdavid <daviti1@mail.com>
2021-12-06 17:44:13 +0100

    Fix tree view dash alignments

--- src/atlasComponents/parcellation/regionHierachy/region-tree/tree-dashes.pipe.ts
+++ src/atlasComponents/parcellation/regionHierachy/region-tree/tree-dashes.pipe.ts
@@ -8,10 +8,12 @@ export class TreeDashesPipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(node: any, isLast: boolean, index: number): SafeStyle {
+  transform(node: any, isLast: boolean, index: number, expanded: boolean): SafeStyle {
     return this.sanitizer.bypassSecurityTrustStyle(`
     ${isLast? 'border-bottom: 1px dashed;' : ''} 
-    ${!node['missBorderPaintingLevels'] || !node['missBorderPaintingLevels'].includes(index+1)? 'border-left: 1px dashed;' : ''}`)
+    ${!node['missBorderPaintingLevels'] || !node['missBorderPaintingLevels'].includes(index+1)? 'border-left: 1px dashed;' : ''}
+    ${node.hasChildren? !expanded? 'margin-top: -9px;' : 'margin-top: -2px;' : 'margin-top: -8px;'}
+    `)
   }
 
 }
