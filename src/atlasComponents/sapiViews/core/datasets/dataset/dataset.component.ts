e0e3c5096f7087555a228d2deba7fe04e20385bd
Xiao Gui <xgui3783@gmail.com>
2022-06-23 14:55:43 +0200

    chore: add restricted access warning

--- src/atlasComponents/sapiViews/core/datasets/dataset/dataset.component.ts
+++ src/atlasComponents/sapiViews/core/datasets/dataset/dataset.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { SapiDatasetModel } from "src/atlasComponents/sapi";
+import { CONST } from "common/constants"
+
+const RESTRICTED_ACCESS_ID = "https://nexus.humanbrainproject.org/v0/data/minds/core/embargostatus/v1.0.0/3054f80d-96a8-4dce-9b92-55c68a8b5efd"
 
 @Component({
   selector: `sxplr-sapiviews-core-datasets-dataset`,
@@ -9,7 +12,17 @@ import { SapiDatasetModel } from "src/atlasComponents/sapi";
   ]
 })
 
-export class DatasetView {
+export class DatasetView implements OnChanges{
   @Input('sxplr-sapiviews-core-datasets-dataset-input')
   dataset: SapiDatasetModel
+
+  public isRestricted = false
+  public CONST = CONST
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { dataset } = changes
+    if (dataset) {
+      this.isRestricted = (dataset.currentValue as SapiDatasetModel)?.metadata?.accessibility?.["@id"] === RESTRICTED_ACCESS_ID
+    }
+  }
 }
