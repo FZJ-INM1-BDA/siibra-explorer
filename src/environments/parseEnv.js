d4999fea6145aaa511eef545766cb18522af7abf
Xiao Gui <xgui3783@gmail.com>
2022-08-29 14:02:49 +0200

    fix git hash and version requirements

--- src/environments/parseEnv.js
+++ src/environments/parseEnv.js
@@ -2,7 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const { promisify } = require('util')
 const asyncWrite = promisify(fs.writeFile)
+const asyncReadFile = promisify(fs.readFile)
 const process = require("process")
+const { exec } = require("child_process")
+
+
+const getGitHead = () => new Promise((rs, rj) => {
+  exec(`git rev-parse --short HEAD`, (err, stdout, stderr) => {
+    if (err) return rj(err)
+    if (stderr) return rj(stderr)
+    rs(stdout)
+  })
+})
+
+const getVersion = async () => {
+  const content = await asyncReadFile("./package.json", "utf-8")
+  const { version } = JSON.parse(content)
+  return version
+}
 
 const main = async () => {
   const target = process.argv[2] || './environment.prod.ts'
@@ -13,29 +30,41 @@ const main = async () => {
     MATOMO_URL,
     MATOMO_ID,
     SIIBRA_API_ENDPOINTS,
-    VERSION,
-    GIT_HASH = 'unknown hash',
     EXPERIMENTAL_FEATURE_FLAG,
     ENABLE_LEAP_MOTION,
   } = process.env
   
+  const version = JSON.stringify(
+    await (async () => {
+      try {
+        return await getVersion()
+      } catch (e) {
+        return "unknown version"
+      }
+    })()
+  )
+  const gitHash = JSON.stringify(
+    await (async () => {
+      try {
+        return await getGitHead()
+      } catch (e) {
+        return "unknown git hash"
+      }
+    })()
+  )
+
   console.log(`[parseEnv.js] parse envvar:`, {
     BACKEND_URL,
     STRICT_LOCAL,
     MATOMO_URL,
     MATOMO_ID,
     SIIBRA_API_ENDPOINTS,
-    VERSION,
-    GIT_HASH,
     EXPERIMENTAL_FEATURE_FLAG,
     ENABLE_LEAP_MOTION,
+
+    VERSION: version,
+    GIT_HASH: gitHash,
   })
-  const version = JSON.stringify(
-    VERSION || 'unknown version'
-  )
-  const gitHash = JSON.stringify(
-    GIT_HASH || 'unknown hash'
-  )
 
   const outputTxt = `
 import { environment as commonEnv } from './environment.common'
