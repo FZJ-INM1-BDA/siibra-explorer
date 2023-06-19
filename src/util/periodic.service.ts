2482109a48dbf4c7e0039af6f577c48d442a0443
Xiao Gui <xgui3783@gmail.com>
2023-06-19 17:49:54 +0200

    maint: simplify periodic service fix: connectivity matrix input fire rate fix: conn busy spinner feat: added selected subject & dataset

--- src/util/periodic.service.ts
+++ src/util/periodic.service.ts
@@ -1,57 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Subject, combineLatest, interval, merge } from "rxjs";
-import { filter, map, scan } from "rxjs/operators";
-import { getUuid } from "./fn";
-
-type Queue = {
-  callback: () => boolean
-  uuid: string
-}
+import { wait } from "./fn";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeriodicSvc{
-  #queue$ = new Subject<Queue>()
-  #dequeue$ = new Subject<Queue>()
-  #scannedQueue$ = merge<{ queue?: Queue, dequeue?: Queue }>(
-    this.#queue$.pipe(
-      map(queue => ({ queue })),
-    ),
-    this.#dequeue$.pipe(
-      map(dequeue => ({ dequeue }))
-    )
-  ).pipe(
-    scan((acc, curr) => {
-      const { queue, dequeue } = curr
-      if (queue) {
-        return [...acc, queue]
+  /**
+   * @description retry a callback until it succeeds
+   * @param callback 
+   */
+  async addToQueue(callback: () => boolean) {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      if (callback()) {
+        break
       }
-      if (dequeue) {
-        return acc.filter(q => q.uuid !== dequeue.uuid)
-      }
-      console.warn(`neither queue nor dequeue were defined!`)
-      return acc
-    }, [] as Queue[])
-  )
-
-  addToQueue(callback: () => boolean){
-    this.#queue$.next({ callback, uuid: getUuid() })
-  }
-  constructor(){
-    combineLatest([
-      this.#scannedQueue$,
-      interval(160)
-    ]).pipe(
-      map(([queues, _]) => queues),
-      filter(queues => queues.length > 0),
-    ).subscribe(queues => {
-      for (const queue of queues) {
-        const { callback } = queue
-        if (callback()) {
-          this.#dequeue$.next(queue)
-        }
-      }
-    })
+      await wait(160)
+    }
   }
 }
