8b3659cb09cea12291dfa7b50865ad6e8daa9a3f
Xiao Gui <xgui3783@gmail.com>
2024-08-05 11:51:02 +0200

    fix cypress e2e

--- cypress/e2e/screenshot.cy.js
+++ cypress/e2e/screenshot.cy.js
@@ -4,15 +4,15 @@ const {
 } = Cypress.env()
 
 describe(`Visiting ${SCREENSHOT_URL}`, () => {
-  beforeAll(() =>  {
+  it(`Screenshot to ${SCREENSHOT_PATH}`, () => {
     if (!SCREENSHOT_URL) {
       console.error(`SCREENSHOT_URL not defined. Exiting`)
       return
     }
+
     cy.visit(SCREENSHOT_URL)
     cy.wait(10000)
-  })
-  it(`Screenshot to ${SCREENSHOT_PATH}`, () => {
+
     if (!SCREENSHOT_PATH) {
       console.error(`SCREENSHOT_PATH not defined. Exiting`)
       return
