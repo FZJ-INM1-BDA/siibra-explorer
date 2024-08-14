33bd53afcb093f80de62a73a019c8ea6f15ddecf
Xiao Gui <xgui3783@gmail.com>
2024-08-14 14:50:03 +0200

    fix screenshot

--- cypress/e2e/screenshot.cy.js
+++ cypress/e2e/screenshot.cy.js
@@ -12,9 +12,9 @@ describe(`Visiting ${SCREENSHOT_URL}`, () => {
 
     cy.visit(SCREENSHOT_URL)
     cy.wait(5000)
-    cy.type('{esc}')
+    cy.get('atlas-viewer').type('{esc}')
     cy.wait(1000)
-    cy.type('{esc}')
+    cy.get('atlas-viewer').type('{esc}')
 
     if (!SCREENSHOT_PATH) {
       console.error(`SCREENSHOT_PATH not defined. Exiting`)
