e4ffed13512ff5b8cca63d8398655d60b02f0d63
Xiao Gui <xgui3783@gmail.com>
2025-02-28 15:48:08 +0100

    debug: screenshot

--- cypress/e2e/screenshot.cy.js
+++ cypress/e2e/screenshot.cy.js
@@ -10,16 +10,66 @@ describe(`Visiting ${SCREENSHOT_URL}`, () => {
       return
     }
 
-    cy.visit(SCREENSHOT_URL)
-    cy.wait(10000)
-    cy.get('body').type('{esc}')
-    cy.wait(1000)
-    cy.get('body').type('{esc}')
+    // monkey patck cypress issue (?) with resize observer
+    // https://github.com/cypress-io/cypress/issues/27415#issuecomment-2169155274
+    cy.on('window:before:load', (win) => {
+      // store real observer
+      const RealResizeObserver = ResizeObserver;
 
-    if (!SCREENSHOT_PATH) {
-      console.error(`SCREENSHOT_PATH not defined. Exiting`)
-      return
-    }
-    cy.screenshot(SCREENSHOT_PATH)
+      let queueFlushTimeout;
+      let queue = [];
+
+      /**
+       * ResizeObserver wrapper with "enforced batches"
+       */
+      class ResizeObserverPolyfill {
+        constructor(callback) {
+          this.callback = callback;
+          this.observer = new RealResizeObserver(this.check.bind(this));
+        }
+
+        observe(element) {
+          this.observer.observe(element);
+        }
+
+        unobserve(element) {
+          this.observer.unobserve(element);
+        }
+
+        disconnect() {
+          this.observer.disconnect();
+        }
+
+        check(entries) {
+          // remove previous invocations of "self"
+          queue = queue.filter((x) => x.cb !== this.callback);
+          // put a new one
+          queue.push({ cb: this.callback, args: entries });
+          // trigger update
+          if (!queueFlushTimeout) {
+            queueFlushTimeout = requestAnimationFrame(() => {
+              queueFlushTimeout = undefined;
+              const q = queue;
+              queue = [];
+              q.forEach(({ cb, args }) => cb(args));
+            }, 0);
+          }
+        }
+      }
+      win.ResizeObserver = ResizeObserverPolyfill
+      
+      cy.visit(SCREENSHOT_URL)
+      cy.wait(10000)
+      cy.get('body').type('{esc}')
+      cy.wait(1000)
+      cy.get('body').type('{esc}')
+
+      if (!SCREENSHOT_PATH) {
+        console.error(`SCREENSHOT_PATH not defined. Exiting`)
+        return
+      }
+      cy.screenshot(SCREENSHOT_PATH)
+   })
+
   })
 })
