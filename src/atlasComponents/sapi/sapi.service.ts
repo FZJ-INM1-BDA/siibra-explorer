2b4168a8f4a68dbdfe13c47ad6f00e7f983f3e86
Xiao Gui <xgui3783@gmail.com>
2025-07-29 16:28:46 +0200

    maint: fix sapi version fix: point assignment width

--- src/atlasComponents/sapi/sapi.service.ts
+++ src/atlasComponents/sapi/sapi.service.ts
@@ -19,7 +19,7 @@ export const useViewer = {
 } as const
 
 export const SIIBRA_API_VERSION_HEADER_KEY='x-siibra-api-version'
-export const EXPECTED_SIIBRA_API_VERSION = '0.3.27'
+export const EXPECTED_SIIBRA_API_VERSION = '0.3.28'
 
 type PaginatedResponse<T> = {
   items: T[]
