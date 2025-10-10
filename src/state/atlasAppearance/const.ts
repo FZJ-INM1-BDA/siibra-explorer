b05b9e881a64727cc19101e98767f51242ad63de
Xiao Gui <xgui3783@gmail.com>
2025-10-10 14:51:51 +0200

    update doc on plugin api

--- src/state/atlasAppearance/const.ts
+++ src/state/atlasAppearance/const.ts
@@ -6,6 +6,7 @@ type CustomLayerBase = {
 }
 
 type NgLayerBase = {
+  id: string
   clType: 'customlayer/nglayer' | 'baselayer/nglayer'
   sxplrAnnotations?: Record<string, string>
 } & CustomLayerBase
